53e9b446f649874e15630bc92285ba68a32f9449
Jozef Maxted <jozef.maxted@theunit.co.uk>
2015-05-22 09:22:26 +0100

    Remove quill instances on directive scope destroy rather than state change

--- src/ng-quill.js
+++ src/ng-quill.js
@@ -182,7 +182,7 @@
 
                     // provide event to get recognized when editor is created -> pass editor object.
                     $timeout(function(){
-                       $scope.$emit('editorCreated', editor); 
+                       $scope.$emit('editorCreated', editor);
                     });
 
                     // set initial value
@@ -212,6 +212,15 @@
                             ngModel.$setViewValue(editor.getHTML());
                         }, 0);
                     });
+
+                    // Destroy the quill instances on destroy
+                    $scope.$on( '$destroy', function() {
+                        if ($window.Quill && $window.Quill.editors && $window.Quill.editors.length) {
+                            angular.forEach($window.Quill.editors, function (editor) {
+                                editor.destroy();
+                            });
+                        }
+                    } );
                 }
             };
         }
@@ -222,16 +231,7 @@
         '$rootScope',
         '$window',
         function ($templateCache, $rootScope, $window) {
-
-            // reset all quill editors (to not flood window object works only with ui-router)
-            $rootScope.$on('$locationChangeSuccess', function () {
-                if ($window.Quill && $window.Quill.editors && $window.Quill.editors.length) {
-                    angular.forEach($window.Quill.editors, function (editor) {
-                        editor.destroy();
-                    });
-                }
-            });
-
+            
             // put template in template cache
             return $templateCache.put('ngQuill/template.html',
                 '<div id="content-container">' +
